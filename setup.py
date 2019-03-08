fe3de85df035ab6b3869ff32cdcca3c841e0c29f
Simon Brunning <simon@brunningonline.net>
2019-03-08 15:25:03 +0000

    Release 0.1.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ except IOError:
 setup(
     name="brunns-builder",
     zip_safe=False,
-    version="0.0.1",
+    version="0.1.0",
     description="Test object builders.",
     long_description=long_description,
     long_description_content_type="text/markdown",
