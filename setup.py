5ef174793b91fcbab46fdb071ace5c17bdf1ecca
Simon Brunning <simon@brunningonline.net>
2020-03-10 10:23:21 +0000

    Bump version number.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ except IOError:
 setup(
     name="brunns-builder",
     zip_safe=False,
-    version="0.3.1",
+    version="0.4.0",
     description="Test object builders.",
     long_description=long_description,
     long_description_content_type="text/markdown",
