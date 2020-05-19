dc862ae6f9c984afe997750a4053893b6544efa5
Simon Brunning <simon@brunningonline.net>
2020-05-19 07:27:51 +0100

    Drop pendulum dependency.

--- setup.py
+++ setup.py
@@ -47,5 +47,5 @@ setup(
         "Topic :: Software Development :: Testing",
     ],
     python_requires=">=3.5",
-    install_requires=["furl~=2.0", "pendulum~=2.0"],
+    install_requires=["furl~=2.0"],
 )
